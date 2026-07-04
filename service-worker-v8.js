9c17d6af0947af1319ea57996c82a65ff2aeb975
liusiyuan965 <liusiyuan965@gmail.com>
2026-07-04 21:49:15 +0900

    Add v8 offline cache

---
+++ service-worker-v8.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "language-growth-v8";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles-v8.css",
+  "./app-v8.js",
+  "./manifest.webmanifest?v=8",
+  "./icon.svg?v=8",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+  if (event.request.method !== "GET" || requestUrl.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html"))),
+  );
+});
